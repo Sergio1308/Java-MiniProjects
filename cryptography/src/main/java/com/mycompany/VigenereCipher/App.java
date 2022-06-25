3121b0cd58d02dfe0092055429ba056fa1deafc9
Sergei Haiduk <sergio13082000@gmail.com>
2022-06-25 23:59:30 +0300

    code improvements

--- cryptography/src/main/java/com/mycompany/VigenereCipher/App.java
+++ cryptography/src/main/java/com/mycompany/VigenereCipher/App.java
@@ -7,7 +7,8 @@ public class App {
 	static String dataSource = "src/test/resources/VigenereTestData/";
 	
 	public static void main(String[] args) {
-        FileResource fr = new FileResource(dataSource + "messages/secretmessage4.txt");  // athens_keyflute.txt
+		// dataSource + "messages/secretmessage4.txt" or "athens_keyflute.txt" like test example
+		FileResource fr = new FileResource();
         VigenereBreaker vb = new VigenereBreaker();
 		vb.breakVigenere(fr);
     }
